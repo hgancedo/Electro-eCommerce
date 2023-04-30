a3e359f46f8dd73d96c24319ce536c1fc2dfa30b
Hector Gancedo Grade <hgancedo@birt.eus>
2023-04-30 10:27:22 +0200

    FINISHED Index.php but just needed to fix Category names on search bar and to change image logo

--- index.php
+++ index.php
@@ -108,12 +108,12 @@ $families = $fam->getFamilies();
               <div class="header-search">
                 <form>
                   <select class="input-select">
-                    <option value="0">All Categories</option>
+                    <option value="0">Categorías</option>
                     <option value="1">Category 01</option>
                     <option value="1">Category 02</option>
                   </select>
-                  <input class="input" placeholder="Search here" />
-                  <button class="search-btn">Search</button>
+                  <input class="input" placeholder="Buscar aquí" />
+                  <button class="search-btn">Buscar</button>
                 </form>
               </div>
             </div>
@@ -126,7 +126,7 @@ $families = $fam->getFamilies();
                 <div>
                   <a href="#">
                     <i class="fa fa-heart-o"></i>
-                    <span>Your Wishlist</span>
+                    <span>Lista Deseos</span>
                     <div class="qty">2</div>
                   </a>
                 </div>
@@ -140,7 +140,7 @@ $families = $fam->getFamilies();
                     aria-expanded="true"
                   >
                     <i class="fa fa-shopping-cart"></i>
-                    <span>Your Cart</span>
+                    <span>Carrito</span>
                     <div class="qty">3</div>
                   </a>
                   <div class="cart-dropdown">
@@ -387,13 +387,13 @@ $families = $fam->getFamilies();
                 <li>
                   <div>
                     <h3>02</h3>
-                    <span>Days</span>
+                    <span>Días</span>
                   </div>
                 </li>
                 <li>
                   <div>
                     <h3>10</h3>
-                    <span>Hours</span>
+                    <span>Horas</span>
                   </div>
                 </li>
                 <li>
@@ -405,13 +405,13 @@ $families = $fam->getFamilies();
                 <li>
                   <div>
                     <h3>60</h3>
-                    <span>Secs</span>
+                    <span>Segs</span>
                   </div>
                 </li>
               </ul>
-              <h2 class="text-uppercase">hot deal this week</h2>
-              <p>New Collection Up to 50% OFF</p>
-              <a class="primary-btn cta-btn" href="#">Shop now</a>
+              <h2 class="text-uppercase">Ofertas Semanales</h2>
+              <p>Descuentos de hasta el 50%</p>
+              <a class="primary-btn cta-btn" href="#">Comprar</a>
             </div>
           </div>
         </div>
@@ -639,15 +639,15 @@ $families = $fam->getFamilies();
         <div class="row">
           <div class="col-md-12">
             <div class="newsletter">
-              <p>Sign Up for the <strong>NEWSLETTER</strong></p>
+              <p>Suscríbete a nuestra <strong>NEWSLETTER</strong></p>
               <form>
                 <input
                   class="input"
                   type="email"
-                  placeholder="Enter Your Email"
+                  placeholder="Introduce tu Email"
                 />
                 <button class="newsletter-btn">
-                  <i class="fa fa-envelope"></i> Subscribe
+                  <i class="fa fa-envelope"></i> Subscribirse
                 </button>
               </form>
               <ul class="newsletter-follow">
@@ -683,7 +683,7 @@ $families = $fam->getFamilies();
           <div class="row">
             <div class="col-md-3 col-xs-6">
               <div class="footer">
-                <h3 class="footer-title">About Us</h3>
+                <h3 class="footer-title">Sobre Nosotros</h3>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
                   do eiusmod tempor incididunt ut.
@@ -691,7 +691,7 @@ $families = $fam->getFamilies();
                 <ul class="footer-links">
                   <li>
                     <a href="#"
-                      ><i class="fa fa-map-marker"></i>1734 Stonecoal Road</a
+                      ><i class="fa fa-map-marker"></i>17 Pol. Industrial Trápaga, Vizcaya</a
                     >
                   </li>
                   <li>
@@ -699,7 +699,7 @@ $families = $fam->getFamilies();
                   </li>
                   <li>
                     <a href="#"
-                      ><i class="fa fa-envelope-o"></i>email@email.com</a
+                      ><i class="fa fa-envelope-o"></i>hgancedo@email.com</a
                     >
                   </li>
                 </ul>
@@ -708,13 +708,14 @@ $families = $fam->getFamilies();
 
             <div class="col-md-3 col-xs-6">
               <div class="footer">
-                <h3 class="footer-title">Categories</h3>
+                <h3 class="footer-title">Categorías</h3>
                 <ul class="footer-links">
-                  <li><a href="#">Hot deals</a></li>
-                  <li><a href="#">Laptops</a></li>
-                  <li><a href="#">Smartphones</a></li>
-                  <li><a href="#">Cameras</a></li>
-                  <li><a href="#">Accessories</a></li>
+                  <li><a href="#">Ofertas</a></li>
+                  <?php foreach($families as $family) {?>
+                  <li><a href="#"><?php echo $family['nombre']; ?></a></li>
+                  <?php
+                  }
+                  ?>
                 </ul>
               </div>
             </div>
@@ -723,26 +724,26 @@ $families = $fam->getFamilies();
 
             <div class="col-md-3 col-xs-6">
               <div class="footer">
-                <h3 class="footer-title">Information</h3>
+                <h3 class="footer-title">Información</h3>
                 <ul class="footer-links">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
-                  <li><a href="#">Orders and Returns</a></li>
-                  <li><a href="#">Terms & Conditions</a></li>
+                  <li><a href="#">Sobre Nosotros</a></li>
+                  <li><a href="#">Contacto</a></li>
+                  <li><a href="#">Política de Privacidad</a></li>
+                  <li><a href="#">Pedidos y Devoluciones</a></li>
+                  <li><a href="#">Términos y Condiciones</a></li>
                 </ul>
               </div>
             </div>
 
             <div class="col-md-3 col-xs-6">
               <div class="footer">
-                <h3 class="footer-title">Service</h3>
+                <h3 class="footer-title">Servicio</h3>
                 <ul class="footer-links">
-                  <li><a href="#">My Account</a></li>
-                  <li><a href="#">View Cart</a></li>
-                  <li><a href="#">Wishlist</a></li>
-                  <li><a href="#">Track My Order</a></li>
-                  <li><a href="#">Help</a></li>
+                  <li><a href="#">Mi Cuenta</a></li>
+                  <li><a href="#">Ver Carrito</a></li>
+                  <li><a href="#">Lista de Deseos</a></li>
+                  <li><a href="#">Seguimiento del Pedido</a></li>
+                  <li><a href="#">Ayuda</a></li>
                 </ul>
               </div>
             </div>
@@ -781,7 +782,7 @@ $families = $fam->getFamilies();
               </ul>
               <span class="copyright">
                 <a target="_blank" href="https://www.templateshub.net"
-                  >Templates Hub</a
+                  >Powered By Hector Gancedo Grade</a
                 >
               </span>
             </div>
